35c58e9a2b9d342eddd60490f53bc711d67fe32b
Naresh <68911787+nareshpoonia@users.noreply.github.com>
2021-10-10 00:42:29 +0530

    collapsible menu css & js

--- index.js
+++ index.js
@@ -1,24 +1,76 @@
 var hamburger = document.querySelector(".hamburger-icon");
 var close = document.querySelector(".close");
-// hamburger.addEventListener("click", clickHandler);
+var collapsibleMenu = document.querySelector(".collapsible-menu");
+var collapsibleMenuCompany = document.querySelector(
+  ".collapsible-menu-company"
+);
+var collapsibleMenuConnect = document.querySelector(
+  ".collapsible-menu-connect"
+);
+var dropDown = document.querySelector(".drop-down");
+var arrowIconImageProduct = document.querySelector(".arrow-icon-image-product");
+var arrowIconImageCompany = document.querySelector(".arrow-icon-image-company");
+var arrowIconImageConnect = document.querySelector(".arrow-icon-image-connect");
 
-// function clickHandler() {
-//   console.log("clicked");
-// }
 let toggleStatus = false;
 
 function toggleChange() {
   if (toggleStatus === false) {
-    console.log("Do Somthing");
     close.style.display = "block";
     hamburger.style.display = "none";
     toggleStatus = true;
-    console.log(toggleStatus);
+    dropDown.style.display = "flex";
   } else if (toggleStatus === true) {
-    console.log("Do Somthing else ");
     close.style.display = "none";
     hamburger.style.display = "block";
     toggleStatus = false;
-    console.log(toggleStatus);
+    dropDown.style.display = "none";
+    collapsibleMenu.style.display = "none";
+    collapsibleMenuCompany.style.display = "none";
+    collapsibleMenuConnect.style.display = "none";
+  }
+}
+
+let collapsibleToggleProduct = false;
+let collapsibleToggleCompany = false;
+let collapsibleToggleConnect = false;
+
+arrowIconImageProduct.addEventListener("click", collapsibleToggleChangeProduct);
+arrowIconImageCompany.addEventListener("click", collapsibleToggleChangeCompany);
+arrowIconImageConnect.addEventListener("click", collapsibleToggleChangeConnect);
+
+function collapsibleToggleChangeProduct() {
+  if (collapsibleToggleProduct === false) {
+    arrowIconImageProduct.style.transform = "rotate(" + 180 + "deg)";
+    collapsibleMenu.style.display = "flex";
+    collapsibleToggleProduct = true;
+  } else if (collapsibleToggleProduct === true) {
+    arrowIconImageProduct.style.transform = "rotate(" + 0 + "deg)";
+    collapsibleMenu.style.display = "none";
+    collapsibleToggleProduct = false;
+  }
+}
+
+function collapsibleToggleChangeCompany() {
+  if (collapsibleToggleCompany === false) {
+    arrowIconImageCompany.style.transform = "rotate(" + 180 + "deg)";
+    collapsibleMenuCompany.style.display = "flex";
+    collapsibleToggleCompany = true;
+  } else if (collapsibleToggleCompany === true) {
+    arrowIconImageCompany.style.transform = "rotate(" + 0 + "deg)";
+    collapsibleMenuCompany.style.display = "none";
+    collapsibleToggleCompany = false;
+  }
+}
+
+function collapsibleToggleChangeConnect() {
+  if (collapsibleToggleConnect === false) {
+    arrowIconImageConnect.style.transform = "rotate(" + 180 + "deg)";
+    collapsibleMenuConnect.style.display = "flex";
+    collapsibleToggleConnect = true;
+  } else if (collapsibleToggleConnect === true) {
+    arrowIconImageConnect.style.transform = "rotate(" + 0 + "deg)";
+    collapsibleMenuConnect.style.display = "none";
+    collapsibleToggleConnect = false;
   }
 }
